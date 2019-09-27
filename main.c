20959ad20cba6f0ba15c93dc20bae682eee0ee62
Seiichi Inoue <d35inou108@gmail.com>
2019-09-28 06:44:25 +0900

    small change on annotation

--- main.c
+++ main.c
@@ -4,19 +4,17 @@ int main(int argc, char **argv) {
   if (argc != 2)
     error("%s: invalid number of arguments", argv[0]);
 
-  // argv[1]をinputに保存
   user_input = argv[1];
 
-  // トークナイズする
+  // Tokenize
   token = tokenize(user_input);
   Node *node = expr();
 
-  // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  // 構文木を下りながらコードを生成
+  // generate assembly code
   gen(node);
 
   printf("  pop rax\n");
