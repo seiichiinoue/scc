2cd7c6d711b8fb27c61986b88deba63d76e9009a
Seiichi Inoue <d35inou108@gmail.com>
2019-10-01 01:06:48 +0900

    added { ... } statement

--- scc.h
+++ scc.h
@@ -60,6 +60,7 @@ typedef enum {
     ND_IF,          // "if"
     ND_WHILE,       // "while"
     ND_FOR,         // "for"
+    ND_BLOCK,       // { ... }
     ND_EXPR_STMT,   // Expression statement
     ND_VAR,         // Variable
     ND_NUM,         // Integer
@@ -80,6 +81,9 @@ struct Node {
     Node *els;
     Node *init;
     Node *inc;
+
+    // block
+    Node *body;
 
     Var *var;       // Used if kind == ND_VAR
     long val;       // Used if kind == ND_NUM
